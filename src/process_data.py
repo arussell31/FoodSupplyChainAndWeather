ff36347b02a392cc144c4117d32f256ff59c5f9f
arussell31 <annarussell0713@gmail.com>
2022-04-06 23:40:05 -0400

    adding missing data

--- src/process_data.py
+++ src/process_data.py
@@ -2,12 +2,20 @@ import numpy as np
 import pandas as pd
 
 def process_temperature_data():
+    # file name contains tas (avg temp), tasmin (min temp), or tasmax (max temp)
+    # row 2 col 2 retrieves the country
+    # row 3 is headers
+    # row 3 col 1 is empty, but represents year
     return
 
 def process_precipitation_data():
+    # row 2 col 2 retrieves the country
+    # row 3 is headers
+    # row 3 col 1 is empty, but represents year
     return
 
 def process_crop_yield_data():
+    # crop name can be extracted from header col 4 or the file name 
     return
 
 def process_agricultural_land_percentage_data():
