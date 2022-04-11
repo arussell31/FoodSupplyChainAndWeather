780550c1cdea0278bbe840b17f63e7ef743c140c
arussell31 <annarussell0713@gmail.com>
2022-04-11 00:07:39 -0400

    Update process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -1,6 +1,8 @@
+from calendar import month
 import string
 from pathlib import Path
 from csv import reader
+import this
 import numpy as np
 import pandas as pd
 import os
@@ -40,6 +42,129 @@ def load_weather_data_from_file(file_path: string, df_columns: list(), prefix=""
                         prev_row = row[1:]
                 df = pd.DataFrame(values, columns=df_columns)
                 df["Country"] = country
+                yield df
+
+        except FileNotFoundError as e:
+            raise e
+
+def load_disaster_data_from_file(file_path: string, df_columns: list()):
+    '''takes in path to folder where data is stored and the header row.
+    yields dataframes from those csv files'''
+    full_path = os.path.join(file_path, "*.csv")
+    csv_files = glob.glob(full_path)
+    for f in csv_files:
+        try:
+            with open(f, newline='') as data_file:
+                csv_reader = reader(data_file, delimiter=',')
+                prev_row = list()
+                values = []
+                for i, row in enumerate(csv_reader):
+                    if i >= 7:
+                        country = row[10].strip()
+                        year = int(row[1].strip())
+                        flood = [False] * 24
+                        drought = [False] * 24
+                        disaster = [False] * 24
+                        total_ppl_affected = 0
+                        to_append = [country, year]
+                        to_append.extend(flood)
+                        to_append.extend(drought)
+                        to_append.extend(disaster)
+                        to_append.append(total_ppl_affected)
+                        flood_start_index = 2
+                        drought_start_index = 26
+                        disaster_start_index = 50
+                        # if col 6, 7, 16, 17 contains disasters or if col 18 contains Yes
+                        # check 28, 29 for start year and start month
+                        # check 31, 32 for end year and end month
+                        # check 34, 38 for total ppl affected
+                        significant_events = \
+                            ["flood", "drought", "tsunami", "famine", "food shortage",
+                            "crop failure", "water shortage"]
+                        event_list =[row[6], row[7], row[16], row[17]]
+                        is_flood = False
+                        is_drought = False
+                        is_other = False
+                        for events in event_list:
+                            if any(x in events.lower() for x in significant_events) or \
+                                ("yes" in row[18].lower()):
+                                if "flood" in events.lower():
+                                    is_flood = True
+                                    break
+                                elif "drought" in events.lower():
+                                    is_drought = True
+                                    break
+                                else:
+                                    is_other = True
+                                    break
+                        if is_flood or is_drought or is_other:
+                            start_year = int(row[28])
+                            start_month = (1 if row[29] == '' else int(row[29]))
+                            end_year = int(row[31])
+                            end_month = (12 if row[32] == '' else int(row[32]))
+                            num_years = 1 + (end_year - start_year)
+                            total_death = (0 if row[34] == '' else int(row[34]))
+                            others_affected = (0 if row[38] == '' else int(row[38]))
+                            total_ppl_affected =  total_death + others_affected
+                            to_append[len(to_append) - 1] = total_ppl_affected
+
+                            type_offset = 2
+                            if is_flood:
+                                type_offset = flood_start_index
+                            if is_drought:
+                                type_offset = drought_start_index
+                            if is_other:
+                                type_offset = disaster_start_index
+                            j = 0
+                            multiple_years = []
+                            while j < num_years + 1:
+                                multiple_years.append(to_append.copy())
+                                j += 1
+                            prev_year = [False] * 12
+                            num_months = ((num_years-1) * 12) + (end_month - start_month + 1)
+                            index = 0
+                            current_month = start_month - 1
+                            while index < len(multiple_years):
+                                this_year = [False] * 12
+                                multiple_years[index][1] = multiple_years[index][1] + index
+                                if index < len(multiple_years) - 1:
+                                    while current_month < (start_month + num_months - 1):
+                                        mod_current_month = current_month%12
+                                        if (mod_current_month != 0 or current_month == 0 or index == current_month/12):
+                                            this_year[mod_current_month] = True
+                                            current_month += 1
+                                        else:
+                                            two_year_data = prev_year.copy()
+                                            two_year_data.extend(this_year)
+                                            for x in range(len(two_year_data)):
+                                                multiple_years[index][type_offset + x] = two_year_data[x]
+                                            prev_year = this_year.copy()
+                                            index += 1
+                                            break
+                                    if current_month == (start_month + num_months - 1):
+                                        two_year_data = prev_year.copy()
+                                        two_year_data.extend(this_year)
+                                        for x in range(len(two_year_data)):
+                                            multiple_years[index][type_offset + x] = two_year_data[x]
+                                        prev_year = this_year.copy()
+                                        index += 1
+
+                                elif index == len(multiple_years) -1:
+                                    two_year_data = prev_year.copy()
+                                    two_year_data.extend(this_year.copy())
+                                    for x in range(len(two_year_data)):
+                                        multiple_years[index][type_offset + x] = two_year_data[x]
+                                    # set the data from previous year
+                                    # set the data from this year
+                                    # no processing needed
+                                    index += 1
+                            values.extend(multiple_years)
+                # before turning values into a df iterate over it and check for any duplicates
+                # where country and year are both equal. 
+                # if there are any. trim it down to one list and add true where necessary
+                # add together the total ppl affected number as well
+                print(values)
+                df = pd.DataFrame(values, columns=df_columns)
                 yield df
 
         except FileNotFoundError as e:
@@ -137,12 +262,44 @@ def process_crop_yield_data():
     return crop_data
 
 def process_agricultural_land_percentage_data():
+    '''processes agricultural data'''
     return
 
 def process_disaster_data():
+    '''processes disaster data'''
+    path = "./data/disaster_data/"
+
     # the file format is xlsx (i did get it converted to csv so will try this first)
     # header row is on row 7
-    return
+
+    disaster_cols = list(["Country", "Year",
+                    "flood_0","flood_1","flood_2","flood_3",
+                    "flood_4","flood_5","flood_6","flood_7",
+                    "flood_8","flood_9","flood_10","flood_11",
+                    "flood_12","flood_13","flood_14","flood_15",
+                    "flood_16","flood_17","flood_18","flood_19",
+                    "flood_20","flood_21","flood_22","flood_23",
+                    "drought_0","drought_1","drought_2","drought_3",
+                    "drought_4","drought_5","drought_6","drought_7",
+                    "drought_8","drought_9","drought_10","drought_11",
+                    "drought_12","drought_13","drought_14","drought_15",
+                    "drought_16","drought_17","drought_18","drought_19",
+                    "drought_20","drought_21","drought_22","drought_23",
+                    "disaster_0","disaster_1","disaster_2","disaster_3",
+                    "disaster_4","disaster_5","disaster_6","disaster_7",
+                    "disaster_8","disaster_9","disaster_10","disaster_11",
+                    "disaster_12","disaster_13","disaster_14","disaster_15",
+                    "disaster_16","disaster_17","disaster_18","disaster_19",
+                    "disaster_20","disaster_21","disaster_22","disaster_23",
+                    "total_ppl_affected"
+                    ])
+    
+    df_list = list(load_disaster_data_from_file(path, disaster_cols))
+    disaster_data = pd.DataFrame()
+    for df in df_list:
+        disaster_data = pd.concat([disaster_data, df], ignore_index=True)
+
+    return disaster_data
 
 def process_grow_season_data():
     # first header col value is empty and is unnecessary as it just represents an index
@@ -152,8 +309,10 @@ def create_dataframe():
     precip_df = process_precipitation_data()
     temp_df = process_temperature_data()
     crop_yield_df = process_crop_yield_data()
+    disaster_df = process_disaster_data()
     weather_df = pd.merge(precip_df, temp_df, on=["Year", "Country"], how='outer')
-    final_df = pd.merge(crop_yield_df, weather_df, on=["Year", "Country"], how='inner')
+    weather_and_crop_df = pd.merge(crop_yield_df, weather_df, on=["Year", "Country"], how='inner')
+    final_df = pd.merge(weather_and_crop_df, disaster_df, on=["Year", "Country"], how='left')
     final_df = final_df.drop(columns=["index"])
     print(final_df)
     weather_filepath = Path('weather_out.csv')
@@ -166,6 +325,7 @@ def create_dataframe():
 
 def main():
     create_dataframe()
+    # process_disaster_data()
 
 if __name__ == "__main__":
     main()
